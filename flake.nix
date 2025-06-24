2d427297700342ac09e1f11ad7d3a1e7dccac8c0
CJ <112206658+Kelpergaming@users.noreply.github.com>
2025-06-24 18:00:40 +0100

    nothing much

--- flake.nix
+++ flake.nix
@@ -2,23 +2,35 @@
   description = "A very basic flake";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+     home-manager = {
+     url = "github:nix-community/home-manager?ref=master"; 
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
       yt-x = {
         url = "github:Benexl/yt-x";
         inputs.nixpkgs.follows = "nixpkgs";
       };  
     trmt.url = "github:cenonym/trmt";
+     nur.url = "github:nix-community/NUR";
   };
-  outputs = { self, nixpkgs, spicetify-nix, trmt, ... }@inputs: 
+  outputs = { self, nixpkgs, spicetify-nix, trmt, home-manager, nur,   ... }@inputs: 
    
   {
        nixosConfigurations.za-warudo = nixpkgs.lib.nixosSystem {
          modules = [
           ({ pkgs, ... }: {
           environment.systemPackages = [ trmt.packages.${pkgs.system}.default ];
+         
         })
+          
           ./configuration.nix
           spicetify-nix.nixosModules.default
+          nur.modules.nixos.default
+          
+         
+         
+        
           ];     
           specialArgs = {
           inherit inputs;
