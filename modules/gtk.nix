63a307fe037728dfa2673194dc40fce716656286
installer <installer@gmail.com>
2025-07-05 16:07:29 +0100

    fixes

--- modules/gtk.nix
+++ modules/gtk.nix
@@ -11,20 +11,24 @@
       package = pkgs.papirus-icon-theme; 
       name = "Papirus"; 
     };
-    
+    gtk3.extraConfig = {
+    "gtk-application-prefer-dark-theme" = true;
   };
-  settings = {
-      gtk-application-prefer-dark-theme = true;
+  gtk4.extraConfig = {
+    "gtk-application-prefer-dark-theme" = true;
   };
 
+    
+  };
+  
     
    dconf = {
     enable = true;
     settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark"; 
-      gtk-theme = "Adwaita-dark"; 
-      icon-theme = "Adwaita";
+      gtk-theme = "adwaita-dark"; 
+      icon-theme = "Papirus";
     };
     };
      };
